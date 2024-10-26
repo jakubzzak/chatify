2bd0aa34019db0d0cdc95e8fdbb66d93d4cea853
andrejzak <andyzak12345@gmail.com>
2024-10-26 14:18:13 +0200

    chat page init

---
+++ client/app/chat/_components/chat.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export function Chat() {
+  return (
+    <div>
+      moj chat
+    </div>
+  )
+}
---
+++ client/app/chat/page.tsx
@@ -0,0 +1,10 @@
+import {Chat} from "@/app/chat/_components/chat";
+
+export default function ChatPage() {
+  return (
+    <div className="flex flex-col min-h-[calc(100vh-8.5rem)] px-4 pb-4 w-full items-center justify-center">
+
+      <Chat />
+    </div>
+  )
+}
