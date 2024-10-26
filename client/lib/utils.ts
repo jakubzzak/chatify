663f22dc7d793b98d308fa4d7eec524f85b4061b
andrejzak <andyzak12345@gmail.com>
2024-10-26 13:40:44 +0200

    add buildMessage function

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -4,3 +4,27 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function buildMessage(message: string, locale: string, args?: {
+  [key: string]: Date | number | string
+}): string {
+  if (!args) {
+    return message
+  }
+  const handledArgs = Object.entries(args)
+    .reduce((obj, [key, value]) => {
+      if (value instanceof Date) {
+        obj[key] = new Intl.DateTimeFormat(locale, {year: 'numeric', month: 'short', day: 'numeric'})
+          .format(new Date(value))
+      } else {
+        obj[key] = value.toString()
+      }
+
+      return obj
+    }, {})
+
+  const injected = message.split(/{(\w+)}/g)
+    .map(part => part in handledArgs ? handledArgs[part] : part)
+
+  return injected.join('')
+}
