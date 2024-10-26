ca9c265e849fe180b294d3ae8ce16a3e19cafd64
andrejzak <andyzak12345@gmail.com>
2024-10-26 14:53:07 +0200

    socket test

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,16 +1,11 @@
-import {Button} from "@/components/ui/button";
-import {ThemeToggle} from "@/app/_providers/theme/theme-toggle";
-import LocaleToggle from "@/app/_providers/intl/locale-toggle";
-import {Message} from "@/app/_providers/intl/message";
+import {TypeWriter} from "@/components/ui/type-writer";
 
 export default function Home() {
   return (
-    <div className="">
-      <ThemeToggle />
-      <LocaleToggle />
-      <Button>
-        <Message value="common.remove" />
-      </Button>
+    <div className="flex flex-col min-h-[calc(100vh-8.5rem)] px-4 pb-4 w-full items-center justify-center">
+      <h1 className="text-2xl md:text-4xl lg:text-6xl font-semibold text-center mb-6">
+        <TypeWriter value="home.title"/>
+      </h1>
     </div>
   );
 }
