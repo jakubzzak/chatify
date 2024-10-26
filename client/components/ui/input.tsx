6086c81368d13d0cd975e0e66d146e6d2544156b
andrejzak <andyzak12345@gmail.com>
2024-10-26 16:43:49 +0200

    refactor input

--- client/components/ui/input.tsx
+++ client/components/ui/input.tsx
@@ -1,15 +1,52 @@
-import * as React from "react"
+"use client"
 
+import * as React from "react"
 import { cn } from "@/lib/utils"
+import {useEffect, useState} from "react";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange"> {
+  timeout?: number,
+  onChange: (value: any) => void,
+  nativeOnChange?: boolean,
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, nativeOnChange, value, timeout = 300, onChange, ...props }, ref) => {
+    let [_value, _onChange] = useState(value)
+
+    useEffect(() => {
+      if (!nativeOnChange)
+        _onChange(value)
+    }, [value])
+
+    useEffect(() => {
+      if (value === _value)
+        return
+
+      if (_value == undefined || (typeof _value === 'string' && _value.length < 1)) {
+        onChange(_value)
+        return
+      }
+
+      const _timeout = setTimeout(() => onChange(_value), timeout)
+
+      return () => clearTimeout(_timeout)
+    }, [_value])
+
+    function handleOnChange(event) {
+      if (nativeOnChange)
+        onChange(event)
+      else if (type === "number")
+        _onChange(event.target.value.length > 0 ? Number(event.target.value) : null)
+      else
+        _onChange(event.target.value)
+    }
+
     return (
       <input
         type={type}
+        onChange={handleOnChange}
         className={cn(
           "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className
