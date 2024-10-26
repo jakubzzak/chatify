3a08f168980fb3688dd708ea2fb53660405ec95e
andrejzak <andyzak12345@gmail.com>
2024-10-26 22:38:36 +0200

    add enableCursor prop

--- client/components/ui/type-writer.tsx
+++ client/components/ui/type-writer.tsx
@@ -1,38 +1,52 @@
-"use client"
+'use client';
 
-import {TypewriterProps, CursorProps, useTypewriter, Cursor} from "react-simple-typewriter";
-import {useMessage} from "@/app/_providers/intl/message";
-import {ReactNode, useState} from "react";
+import {
+  TypewriterProps,
+  CursorProps,
+  useTypewriter,
+  Cursor,
+} from 'react-simple-typewriter';
+import { useMessage } from '@/app/_providers/intl/message';
+import { ReactNode, useState } from 'react';
 
 type TypeWriterProps = {
-  value: string
-  cursor?: boolean
-  cursorStyle?: string | ReactNode
-  skeleton?: ReactNode
-} & Omit<TypewriterProps, "words" | "onLoopDone"> & Omit<CursorProps, "cursorStyle">
+  value: string;
+  cursor?: boolean;
+  cursorStyle?: string | ReactNode;
+  skeleton?: ReactNode;
+  enableCursor?: boolean;
+} & Omit<TypewriterProps, 'words' | 'onLoopDone'> &
+  Omit<CursorProps, 'cursorStyle'>;
 
 export function TypeWriter({
-                             value,
-                             cursorStyle = "|",
-                             skeleton,
-                             cursorBlinking = true,
-                             cursorColor,
-                             cursor = true,
-                             ...props
-                           }: TypeWriterProps) {
-  const t = useMessage()
-  const [enableCursor, setEnableCursor] = useState(true)
+  value,
+  cursorStyle = '|',
+  skeleton,
+  cursorBlinking = true,
+  cursorColor,
+  cursor = true,
+  enableCursor = true,
+  ...props
+}: TypeWriterProps) {
+  const t = useMessage();
+  const [_enableCursor, setEnableCursor] = useState(true);
 
   const [text] = useTypewriter({
     words: [t(value)],
     onLoopDone: () => setTimeout(() => setEnableCursor(false), 3000),
     ...props,
-  })
+  });
 
   return (
     <>
       {text}
-      {enableCursor && <Cursor cursorBlinking={cursorBlinking} cursorStyle={cursorStyle} cursorColor={cursorColor}/>}
+      {enableCursor && _enableCursor && (
+        <Cursor
+          cursorBlinking={cursorBlinking}
+          cursorStyle={cursorStyle}
+          cursorColor={cursorColor}
+        />
+      )}
     </>
-  )
+  );
 }
