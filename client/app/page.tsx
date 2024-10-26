fb5ff60fdf5293910a229ee432e59eb57dba724f
andrejzak <andyzak12345@gmail.com>
2024-10-26 13:42:43 +0200

    update page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,15 @@
 import {Button} from "@/components/ui/button";
 import {ThemeToggle} from "@/app/_providers/theme/theme-toggle";
+import LocaleToggle from "@/app/_providers/intl/locale-toggle";
+import {Message} from "@/app/_providers/intl/message";
 
 export default function Home() {
   return (
     <div className="">
       <ThemeToggle />
+      <LocaleToggle />
       <Button>
-        test
+        <Message value="common.remove" />
       </Button>
     </div>
   );
