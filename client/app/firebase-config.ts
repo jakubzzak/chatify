42f544374f5d556cc23cb699e4fd19f4ec23948d
andrejzak <andyzak12345@gmail.com>
2024-10-27 17:30:24 +0100

    refactor firebase config

--- client/app/firebase-config.ts
+++ client/app/firebase-config.ts
@@ -1,15 +1,9 @@
 import { initializeApp } from '@firebase/app';
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyD2i5M5aadFAA_agiEaDik174OSZc0quu0',
-  authDomain: 'chatify-2024-10.firebaseapp.com',
-  projectId: 'chatify-2024-10',
-  storageBucket: 'chatify-2024-10.appspot.com',
-  messagingSenderId: '884674076348',
-  appId: '1:884674076348:web:fcb8807aa923ec8e383ab9',
-  measurementId: 'G-DK7SFFLDCV',
-};
+const firebaseConfig = process.env.NEXT_PUBLIC_FIREBASE_CONFIG
+  ? JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG)
+  : {};
 
 const app = initializeApp(firebaseConfig);
 
