49724676ff9b7758e5de2116fa6ebc168f5a43fb
andrejzak <andyzak12345@gmail.com>
2024-10-27 17:19:18 +0100

    add new field

--- client/app/room/_components/schema.ts
+++ client/app/room/_components/schema.ts
@@ -2,6 +2,9 @@ import { object, string } from '@/lib/yup';
 
 export const CreateRoomSchema = object({
   name: string().required().default(null),
+  username: string().required().default(null),
+  isPersistent: string().required().default('true'),
+  isPrivate: string().required().default('true'),
 });
 
 export const JoinRoomSchema = object({
