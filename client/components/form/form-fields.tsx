c4331d8fa8bee47d79e7f5eb58396e25888758e7
andrejzak <andyzak12345@gmail.com>
2024-10-27 17:23:30 +0100

    formmed componets

--- client/components/form/form-fields.tsx
+++ client/components/form/form-fields.tsx
@@ -1,6 +1,10 @@
 'use client';
 
-import { MessageType } from '@/app/_providers/intl/message';
+import {
+  MessageFunction,
+  MessageType,
+  useMessage,
+} from '@/app/_providers/intl/message';
 import { ReactNode } from 'react';
 import { Input } from '@/components/ui/input';
 import { useController } from 'react-hook-form';
@@ -12,6 +16,7 @@ import {
   FormMessage,
   FormFieldContext,
 } from '@/components/form/index';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 type FormedInputProps<T extends (...args: any) => any> = Omit<
   Parameters<T>[0],
@@ -25,7 +30,57 @@ type FormedInputProps<T extends (...args: any) => any> = Omit<
 
 type FormedValueItem = {
   value: string;
-  label?: ReactNode | string;
+  label?: ReactNode | MessageType | ((t: MessageFunction) => string);
+};
+
+export const FormedTabs = (
+  props: FormedInputProps<typeof Tabs> & { values: FormedValueItem[] },
+) => {
+  const {
+    field: { onChange, value },
+    formState,
+  } = useController(props);
+  const t = useMessage();
+
+  function handleOnChange(...args) {
+    if (!formState.disabled) onChange(...args);
+  }
+
+  return (
+    <FormFieldContext.Provider value={{ name: props.name }}>
+      <FormItem>
+        {props.label && <FormLabel value={props.label} />}
+        <FormControl>
+          <Tabs value={value} onValueChange={handleOnChange}>
+            <TabsList>
+              {props.values.map(({ value, label }) => {
+                if (typeof label === 'function') {
+                  label = label(t);
+                } else if (typeof label === 'string') {
+                  label = t(label);
+                } else if (!label) {
+                  label = value;
+                }
+
+                return (
+                  <TabsTrigger
+                    key={value}
+                    {...{
+                      value,
+                      children: label,
+                      disabled: formState.disabled,
+                    }}
+                  />
+                );
+              })}
+            </TabsList>
+          </Tabs>
+        </FormControl>
+        {props.description && <FormDescription value={props.description} />}
+        <FormMessage />
+      </FormItem>
+    </FormFieldContext.Provider>
+  );
 };
 
 export const FormedInput = (props: FormedInputProps<typeof Input>) => {
