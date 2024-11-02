66a7b56a4ffcd61277fa7368d3f46574ef37b815
andrejzak <andyzak12345@gmail.com>
2024-11-02 19:32:02 +0100

    refactor useState to useMemo

--- client/components/form/form-fields.tsx
+++ client/components/form/form-fields.tsx
@@ -17,6 +17,12 @@ import {
   FormFieldContext,
 } from '@/components/form/index';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+} from '@/components/ui/select';
 
 type FormedInputProps<T extends (...args: any) => any> = Omit<
   Parameters<T>[0],
@@ -33,6 +39,26 @@ type FormedValueItem = {
   label?: ReactNode | MessageType | ((t: MessageFunction) => string);
 };
 
+const Label = ({
+  label,
+  value,
+}: {
+  value?: string;
+  label: ReactNode | MessageType | ((t: MessageFunction) => string);
+}) => {
+  const t = useMessage();
+
+  if (typeof label === 'function') {
+    label = label(t);
+  } else if (typeof label === 'string') {
+    label = t(label);
+  } else if (!label) {
+    label = value;
+  }
+
+  return label;
+};
+
 export const FormedTabs = (
   props: FormedInputProps<typeof Tabs> & { values: FormedValueItem[] },
 ) => {
@@ -40,7 +66,6 @@ export const FormedTabs = (
     field: { onChange, value },
     formState,
   } = useController(props);
-  const t = useMessage();
 
   function handleOnChange(...args) {
     if (!formState.disabled) onChange(...args);
@@ -53,28 +78,56 @@ export const FormedTabs = (
         <FormControl>
           <Tabs value={value} onValueChange={handleOnChange}>
             <TabsList>
-              {props.values.map(({ value, label }) => {
-                if (typeof label === 'function') {
-                  label = label(t);
-                } else if (typeof label === 'string') {
-                  label = t(label);
-                } else if (!label) {
-                  label = value;
-                }
-
-                return (
-                  <TabsTrigger
-                    key={value}
-                    {...{
-                      value,
-                      children: label,
-                      disabled: formState.disabled,
-                    }}
-                  />
-                );
-              })}
+              {props.values.map(({ value, label }) => (
+                <TabsTrigger
+                  key={value}
+                  {...{
+                    value,
+                    children: <Label {...{ label, value }} />,
+                    disabled: formState.disabled,
+                  }}
+                />
+              ))}
             </TabsList>
           </Tabs>
+        </FormControl>
+        {props.description && <FormDescription value={props.description} />}
+        <FormMessage />
+      </FormItem>
+    </FormFieldContext.Provider>
+  );
+};
+
+export const FormedSelect = (
+  props: FormedInputProps<typeof Select> & { values: FormedValueItem[] },
+) => {
+  const {
+    field: { onChange, value },
+    formState,
+  } = useController(props);
+
+  function handleOnChange(...args) {
+    if (!formState.disabled) onChange(...args);
+  }
+
+  return (
+    <FormFieldContext.Provider value={{ name: props.name }}>
+      <FormItem>
+        {props.label && <FormLabel value={props.label} />}
+        <FormControl>
+          <Select onValueChange={handleOnChange}>
+            <SelectTrigger>
+              {/* @ts-ignore */}
+              {props.values.find((item) => item.value === value)?.label}
+            </SelectTrigger>
+            <SelectContent>
+              {props.values.map(({ value, label }) => (
+                <SelectItem key={value} value={value}>
+                  <Label {...{ label, value }} />
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
         </FormControl>
         {props.description && <FormDescription value={props.description} />}
         <FormMessage />
