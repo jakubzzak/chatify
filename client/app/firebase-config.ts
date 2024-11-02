52fb96f7f8a9fce662f046cccd78fa6025096888
andrejzak <andyzak12345@gmail.com>
2024-11-02 13:40:42 +0100

    refactor firebase config

--- client/app/firebase-config.ts
+++ client/app/firebase-config.ts
@@ -1,9 +1,7 @@
 import { initializeApp } from '@firebase/app';
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = process.env.NEXT_PUBLIC_FIREBASE_CONFIG
-  ? JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG)
-  : {};
+const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG);
 
 const app = initializeApp(firebaseConfig);
 
