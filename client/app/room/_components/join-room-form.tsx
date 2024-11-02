2ac4f6edd9ba352dfd792f9ae212a231a0757185
andrejzak <andyzak12345@gmail.com>
2024-11-02 22:01:18 +0100

    add join form to dialog

--- client/app/room/_components/join-room-form.tsx
+++ client/app/room/_components/join-room-form.tsx
@@ -1,12 +1,5 @@
 'use client';
 
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
 import { Message } from '@/app/_providers/intl/message';
 import { Button } from '@/components/ui/button';
 import { Form, useForm } from '@/components/form';
@@ -20,6 +13,16 @@ import {
 import { useFetch, useFetcher } from '@/lib/hooks/use-fetch';
 import { InferType } from '@/lib/yup';
 import { useRouter } from 'next/navigation';
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { LogIn } from 'lucide-react';
+import { useState } from 'react';
 
 export function JoinRoomForm() {
   const form = useForm({
@@ -30,23 +33,39 @@ export function JoinRoomForm() {
   const { data, loading } = useFetch<Room[]>('/rooms?excludeSelf=true');
   const { fetcher, errorHandler } = useFetcher();
   const router = useRouter();
+  const [open, setOpen] = useState(false);
 
   const onSubmit = (data: InferType<typeof JoinRoomSchema>) => {
     return fetcher('/rooms/join', { method: 'POST', body: data })
-      .then((res) => router.push(res?.id))
+      .then((res) => {
+        setOpen(false);
+        router.push(res?.id);
+      })
       .catch(errorHandler);
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <Card>
-          <CardHeader>
-            <CardTitle>
-              <Message value="chatRoom.joinExisting" />
-            </CardTitle>
-          </CardHeader>
-          <CardContent className="space-y-2">
+    <Dialog onOpenChange={(_open) => setOpen(_open)} open={open}>
+      <DialogTrigger asChild>
+        <Button
+          size="icon"
+          variant="default"
+          className="h-8 w-8"
+          onClick={() => setOpen(true)}>
+          <LogIn />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="text-2xl">
+            <Message value="chatRoom.joinExisting" />
+          </DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form
+            id="join-form"
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4">
             <FormedTabs
               name="type"
               values={[
@@ -74,17 +93,18 @@ export function JoinRoomForm() {
                 }
               />
             )}
-          </CardContent>
-          <CardFooter>
-            <Button
-              className="w-full"
-              type="submit"
-              loading={form.formState.isSubmitting}>
-              <Message value="common.submit" />
-            </Button>
-          </CardFooter>
-        </Card>
-      </form>
-    </Form>
+          </form>
+        </Form>
+        <DialogFooter>
+          <Button
+            form="join-form"
+            className="w-full"
+            type="submit"
+            loading={form.formState.isSubmitting}>
+            <Message value="common.submit" />
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
