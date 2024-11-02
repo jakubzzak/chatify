521c06e426a88e1512da3a5ab010953faa5d6a8b
andrejzak <andyzak12345@gmail.com>
2024-11-02 13:31:25 +0100

    add toast component

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,34 +1,41 @@
-import { IntlProvider } from "@/app/_providers/intl/intl-provider";
-import { SWRProvider } from "@/app/_providers/swr";
-import { ThemeProvider } from "@/app/_providers/theme/theme-provider";
-import { Footer } from "@/components/footer";
-import { Navbar } from "@/components/navbar";
-import type { Metadata } from "next";
-import { ReactNode } from "react";
-import "./globals.css";
+import { IntlProvider } from '@/app/_providers/intl/intl-provider';
+import { SWRProvider } from '@/app/_providers/swr';
+import { ThemeProvider } from '@/app/_providers/theme/theme-provider';
+import { Footer } from '@/components/footer';
+import { Navbar } from '@/components/navbar';
+import type { Metadata } from 'next';
+import { ReactNode } from 'react';
+import './globals.css';
+import { Toaster } from '@/components/ui/toast/toaster';
 
 export const metadata: Metadata = {
-  title: "Chatify",
-  description: "Your one and only chatting app",
+  title: 'Chatify',
+  description: 'Your one and only chatting app',
 };
 
-export default function RootLayout({children}: Readonly<{ children: ReactNode }>) {
-
+export default function RootLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="en">
-    <body className="antialiased">
-    <SWRProvider>
-      <IntlProvider>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <main className="min-w-screen min-h-screen pt-[4.5rem] relative">
-            <Navbar/>
-            {children}
-            <Footer/>
-          </main>
-        </ThemeProvider>
-      </IntlProvider>
-    </SWRProvider>
-    </body>
+      <body className="antialiased">
+        <SWRProvider>
+          <IntlProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange>
+              <main className="min-w-screen min-h-screen pt-[4.5rem] relative">
+                <Navbar />
+                {children}
+                <Footer />
+              </main>
+              <Toaster />
+            </ThemeProvider>
+          </IntlProvider>
+        </SWRProvider>
+      </body>
     </html>
   );
 }
