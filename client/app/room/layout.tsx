1b403f1fcc52d26c236fae95574083226445ea25
andrejzak <andyzak12345@gmail.com>
2024-11-02 19:38:26 +0100

    refactor layout

--- client/app/room/layout.tsx
+++ client/app/room/layout.tsx
@@ -1,5 +1,13 @@
 import { AuthProvider } from '@/app/_providers/auth';
+import { Navbar } from '@/components/navbar';
 
 export default function AuthLayout({ children }) {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <AuthProvider>
+      <Navbar variant="private" />
+      <div className="flex flex-col min-h-[calc(100vh-4.5rem)] px-4 pb-4 w-full items-center justify-center">
+        {children}
+      </div>
+    </AuthProvider>
+  );
 }
