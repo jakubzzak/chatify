58a4cb4c0586d2e875395418ab37dfd222c06879
andrejzak <andyzak12345@gmail.com>
2024-11-03 11:40:40 +0100

    add create room form to dialog

--- client/app/room/_components/create-room-form.tsx
+++ client/app/room/_components/create-room-form.tsx
@@ -1,21 +1,25 @@
 'use client';
 
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
 import { Message } from '@/app/_providers/intl/message';
 import { Button } from '@/components/ui/button';
 import { Form, useForm } from '@/components/form';
 import { CreateRoomSchema } from '@/app/room/_components/schema';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { FormedInput, FormedTabs } from '@/components/form/form-fields';
-import { fetchApi } from '@/lib/fetch';
 import { useRouter } from 'next/navigation';
 import { useAuth } from '@/app/_providers/auth';
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Plus } from 'lucide-react';
+import { useFetcher, useRevalidate } from '@/lib/hooks/use-fetch';
+import { useState } from 'react';
+import { InferType } from 'yup';
 
 export function CreateRoomForm() {
   const form = useForm({
@@ -24,43 +28,66 @@ export function CreateRoomForm() {
   });
   const router = useRouter();
   const { token } = useAuth();
+  const { fetcher, errorHandler } = useFetcher();
+  const mutate = useRevalidate();
+  const [open, setOpen] = useState(false);
 
-  const onSubmit = async (data) => {
+  const onSubmit = async (data: InferType<typeof CreateRoomSchema>) => {
     const _data = {
       ...data,
       isPersistent: data.isPersistent === 'true',
       isPrivate: data.isPrivate === 'true',
     };
 
-    return fetchApi('/rooms', {
+    return fetcher('/rooms', {
       method: 'POST',
       body: _data,
       token,
     })
       .then((res) => {
-        router.push(`/room/${res.id}?username=${data.username}`);
+        setOpen(false);
+        mutate('/profile');
+        router.push(`/room/${res.id}`);
       })
-      .catch(() => console.log('eeeeeeeee'));
+      .catch(errorHandler);
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <Card>
-          <CardHeader>
-            <CardTitle>
-              <Message value="chatRoom.createNew" />
-            </CardTitle>
-          </CardHeader>
-          <CardContent className="space-y-2">
+    <Dialog onOpenChange={(_open) => setOpen(_open)} open={open}>
+      <DialogTrigger asChild>
+        <Button
+          size="icon"
+          variant="default"
+          className="h-8 w-8"
+          onClick={() => setOpen(true)}>
+          <Plus />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="text-2xl">
+            <Message value="chatRoom.createNew" />
+          </DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            id="create-room-form"
+            className="space-y-4">
             <FormedInput name="name" label="chatRoom.name" />
             <div className="flex gap-2">
               <FormedTabs
                 className=""
                 name="isPersistent"
                 values={[
-                  { value: 'true', label: 'chatRoom.conversation.persistent' },
-                  { value: 'false', label: 'chatRoom.conversation.temporary' },
+                  {
+                    value: 'true',
+                    label: 'chatRoom.conversation.persistent',
+                  },
+                  {
+                    value: 'false',
+                    label: 'chatRoom.conversation.temporary',
+                  },
                 ]}
                 label="chatRoom.conversation"
               />
@@ -73,17 +100,18 @@ export function CreateRoomForm() {
                 label="chatRoom.roomType"
               />
             </div>
-          </CardContent>
-          <CardFooter>
-            <Button
-              className="w-full"
-              type="submit"
-              loading={form.formState.isSubmitting}>
-              <Message value="common.submit" />
-            </Button>
-          </CardFooter>
-        </Card>
-      </form>
-    </Form>
+          </form>
+        </Form>
+        <DialogFooter>
+          <Button
+            form="create-room-form"
+            className="w-full"
+            type="submit"
+            loading={form.formState.isSubmitting}>
+            <Message value="common.submit" />
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
